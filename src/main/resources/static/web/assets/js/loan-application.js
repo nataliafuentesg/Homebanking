fdef726f9f930bd049af92afef814562207f9210
NATALIAFUENTES <nataliaf1412@gmail.com>
2023-11-11 21:58:32 -0500

    New front design, User dashboard

--- src/main/resources/static/web/assets/js/loan-application.js
+++ src/main/resources/static/web/assets/js/loan-application.js
@@ -14,6 +14,7 @@ const options = {
             accounts: [],
             resultMessage: '',
             firstName: '',
+            loans: [],
 
 
         }
@@ -27,6 +28,8 @@ const options = {
                 console.log(this.firstName)
                 this.accounts = response.data.accounts
                 console.log(this.accounts)
+                this.loans = response.data.loans
+                console.log(this.loans)
 
             })
             .catch(error => console.log(error));
